3fc0619eb9c95b22b26d04faf2a7c2e0bd163708
Kacper Skrodzki <kacskrogry@gmail.com>
2025-11-20 19:19:19 +0100

    fixes in games history service

--- backend/src/main/java/pap/project/game_history/GameHistoryService.java
+++ backend/src/main/java/pap/project/game_history/GameHistoryService.java
@@ -6,7 +6,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
+import org.springframework.transaction.annotation.Transactional;
+import pap.project.game_history.controller.SaveRequest;
 import pap.project.users.User;
+import pap.project.users.UserRepository;
 
 @Service
 public class GameHistoryService {
@@ -14,39 +17,39 @@ public class GameHistoryService {
     private static final String LOG_PREFIX = "-gameHistoryService";
     private static final Logger LOG = LoggerFactory.getLogger(GameHistoryService.class);
 
-    private final @NonNull MatchRepository matchRepository;
+    private static final Integer ELO_DOWN = -10;
+    private static final Integer ELO_UP = 20;
 
-    public GameHistoryService(@NonNull MatchRepository matchRepository)
+    private final @NonNull MatchRepository matchRepository;
+    private final UserRepository userRepository;
+
+    public GameHistoryService(@NonNull MatchRepository matchRepository, UserRepository userRepository)
     {
         this.matchRepository = matchRepository;
+        this.userRepository = userRepository;
     }
 
     /**
      * @return information if match was successfully saved
      */
-    public @NonNull SaveResult saveMatch(
-            @NonNull User winner,
-            @NonNull User loser,
-            @NonNull Long finishTime,
-            @NonNull Integer winnerEloChange,
-            @NonNull Integer loserEloChange
-        )
+    @Transactional
+    public @NonNull SaveResult saveMatch(@NonNull String logPrefix, @NonNull SaveRequest saveRequest)
     {
         final Match match = new Match(
-                winner,
-                loser,
-                finishTime,
-                winnerEloChange,
-                loserEloChange
+                userRepository.findById(saveRequest.winnerId()).get(),
+                userRepository.findById(saveRequest.loserId()).get(),
+                saveRequest.finishTime(),
+                ELO_UP,
+                ELO_DOWN
         );
         try
         {
             matchRepository.save(match);
-            LOG.info("%s match %s saved to database".formatted(LOG_PREFIX, match.getId()));
+            LOG.info("%s match %s saved to database".formatted(LOG_PREFIX, match.getmatchId()));
             return SaveResult.SUCCESS;
         } catch (PersistenceException exit)
         {
-            LOG.warn("%s match id: %s  error saving to database".formatted(LOG_PREFIX, match.getId()), exit);
+            LOG.warn("%s match id: %s  error saving to database".formatted(LOG_PREFIX, match.getmatchId()), exit);
             return SaveResult.FAILED;
         }
     }
