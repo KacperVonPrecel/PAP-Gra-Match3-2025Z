f67f60803f845f0ea982e33bcc0b1b2d298b94cf
Kacper Skrodzki <kacskrogry@gmail.com>
2025-11-20 19:44:30 +0100

    added rank, total games, total wins and currency fields to user

--- backend/src/main/java/pap/project/users/User.java
+++ backend/src/main/java/pap/project/users/User.java
@@ -15,6 +15,11 @@ import java.util.OptionalLong;
 )
 public class User
 {
+    private static final Integer STARTING_CURRENCY = 1000;
+    private static final Integer STARTING_RANK = 100;
+    private static final Integer STARTING_TOTAL_GAMES = 0;
+    private static final Integer STARTING_TOTAL_WINS = 0;
+
     @Id
     @GeneratedValue
     private Long id;
@@ -23,6 +28,11 @@ public class User
     private String email;
     private String hashedPassword;
 
+    private Integer rank;
+    private Integer totalGames;
+    private Integer totalWins;
+    private Integer currency;
+
     protected User() {}
 
     public User(@NonNull String username, @NonNull String email, @NonNull String hashedPassword)
@@ -30,6 +40,10 @@ public class User
         this.username = Objects.requireNonNull(username);
         this.email = Objects.requireNonNull(email);
         this.hashedPassword = Objects.requireNonNull(hashedPassword);
+        this.rank = STARTING_RANK;
+        this.totalGames = STARTING_TOTAL_GAMES;
+        this.totalWins = STARTING_TOTAL_WINS;
+        this.currency = STARTING_CURRENCY;
     }
 
     public @NonNull OptionalLong getId()
@@ -50,5 +64,24 @@ public class User
     public @NonNull String getHashedPassword()
     {
         return Objects.requireNonNull(hashedPassword);
+    }
+
+    public @NonNull Integer getRank() {
+        return Objects.requireNonNull(rank);
+    }
+
+    public @NonNull Integer getTotalGames()
+    {
+        return Objects.requireNonNull(totalGames);
+    }
+
+    public @NonNull Integer getTotalWins()
+    {
+        return Objects.requireNonNull(totalWins);
+    }
+
+    public @NonNull Integer getCurrency()
+    {
+        return Objects.requireNonNull(currency);
     }
 }
