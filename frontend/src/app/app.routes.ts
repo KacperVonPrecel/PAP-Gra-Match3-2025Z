a1dbbed7fc5d32923a97279ab25c3e52b14acdd0
Wiktoria Parzych <parzych.wiktoria@gmail.com>
2025-11-30 14:28:56 +0100

    Initialised home page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Register } from './auth/register/register';
 import { Login } from './auth/login/login';
+import { HomePage } from './home/home-page/home-page';
 
 export const routes: Routes = [
     {
@@ -12,5 +13,10 @@ export const routes: Routes = [
         path: 'auth/login',
         component: Login,
         title: 'Login'
+    },
+    {
+        path: 'home/home-page',
+        component: HomePage,
+        title: 'HomePage'
     }
 ];
