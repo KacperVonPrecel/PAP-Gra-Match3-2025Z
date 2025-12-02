3c17d3bf4f4f827c9169c139791024263f7e8c35
Norbert <54719382+NorbertCode@users.noreply.github.com>
2025-12-03 00:21:16 +0100

    service and controller for match3 game

---
+++ backend/src/main/java/pap/project/match3/Match3Controller.java
@@ -0,0 +1,44 @@
+package pap.project.match3;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Match3Controller
+{
+    private final Match3Service service;
+
+    public  Match3Controller(Match3Service service)
+    {
+        this.service = service;
+        service.startNewGame();
+    }
+
+    @MessageMapping("/board/{gameId}/state")
+    @SendTo("/topic/board/{gameId}/state")
+    public Match3Block[][] updateBoard(@DestinationVariable Integer gameId)
+    {
+        return service.getBoard(gameId).getBlocks();
+    }
+
+    @MessageMapping("/board/{gameId}/swap")
+    public boolean swapBlocks(@DestinationVariable Integer gameId, Match3Board.MoveRequest moveRequest)
+    {
+        // TODO: Add bound-checks, if the request fits the board
+        return service.swapBlocks(gameId, moveRequest);
+    }
+
+    @MessageMapping("/board/{gameId}/destroyMatchedBlocks")
+    public void destroyMatchedBlocks(@DestinationVariable Integer gameId)
+    {
+        service.destroyMatchedBlocks(gameId);
+    }
+
+    @MessageMapping("/board/{gameId}/fillInBlocks")
+    public void fillInBlocks(@DestinationVariable Integer gameId)
+    {
+        service.fillInBlocks(gameId);
+    }
+}
