d71c1b8c34f7c4edf18f238d0c89639b73cb0c5c
Norbert <54719382+NorbertCode@users.noreply.github.com>
2025-12-03 00:18:17 +0100

    websocket config

---
+++ backend/src/main/java/pap/project/config/WebSocketSecurityConfig.java
@@ -0,0 +1,14 @@
+package pap.project.config;
+
+//XXX https://docs.spring.io/spring-security/reference/servlet/integrations/websocket.html
+//@Configuration
+//@EnableWebSocketSecurity
+public class WebSocketSecurityConfig
+{
+//    @Bean
+//    @NonNull AuthorizationManager<Message<?>> messageAuthorizationManager(@NonNull MessageMatcherDelegatingAuthorizationManager.Builder messages) {
+////        messages.simpDestMatchers("/user/**").hasRole("USER")
+////XXX
+//        return messages.build();
+//    }
+}
