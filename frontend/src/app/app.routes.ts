7d5d8026e5c0e46f41996d7fd6edbf1563d5fae9
Norbert <54719382+NorbertCode@users.noreply.github.com>
2025-12-03 00:20:00 +0100

    example frontend for match3 game

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Register } from './auth/register/register';
 import { Login } from './auth/login/login';
+import { Match3 } from './match3/match3/match3';
 
 export const routes: Routes = [
     {
@@ -12,5 +13,10 @@ export const routes: Routes = [
         path: 'auth/login',
         component: Login,
         title: 'Login'
+    },
+    {
+        path: 'match3',
+        component: Match3,
+        title: 'Match3'
     }
 ];
