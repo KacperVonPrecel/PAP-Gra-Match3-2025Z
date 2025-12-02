3e44906a2ac0cae7b6ba1a89f1187d8660531130
Norbert <54719382+NorbertCode@users.noreply.github.com>
2025-12-03 00:20:55 +0100

    swapping and finding matches functionality

--- backend/src/main/java/pap/project/match3/Match3Block.java
+++ backend/src/main/java/pap/project/match3/Match3Block.java
@@ -1,20 +1,25 @@
 package pap.project.match3;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.lang.NonNull;
 
 public class Match3Block
 {
-    @NonNull private BlockType blockType = BlockType.None;
+    @NonNull private BlockType blockType = BlockType.Empty;
 
+    // TODO: Block type for disabled block (different board shapes support)
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER)
     public enum BlockType
     {
-        None,
+        Disabled,
+        Empty,
         Red,
         Green,
         Blue,
         Yellow,
     }
 
+    public Match3Block() { }
     public Match3Block(@NonNull BlockType blockType)
     {
         this.blockType = blockType;
--- backend/src/main/java/pap/project/match3/Match3Board.java
+++ backend/src/main/java/pap/project/match3/Match3Board.java
@@ -1,5 +1,7 @@
 package pap.project.match3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Match3Board
@@ -9,6 +11,8 @@ public class Match3Board
 
     private final Random random = new Random();
 
+    public record MoveRequest(int sourceRow, int sourceColumn, int targetRow, int targetColumn) { };
+
     public Match3Board(Match3Block[][] board, MatchableShape[] matchableShapes)
     {
         this.board = board;
@@ -17,50 +21,126 @@ public class Match3Board
         fillInBlocks();
     }
 
-    public Match3Block[][] getBoard()
+    public Match3Block[][] getBlocks()
     {
         return board;
     }
 
-    public void swapBlocks(int source_row, int source_col, int target_row, int target_col)
+    public boolean swapBlocks(MoveRequest moveRequest)
     {
-        Match3Block temp = board[source_row][source_col];
+        // TODO: Add check if swap causes matches
+        if (!areBlocksSwappable(moveRequest))
+            return false;
 
-        board[source_row][source_col] = board[target_row][target_col];
-        board[target_row][target_col] = temp;
-    }
+        Match3Block temp = board[moveRequest.sourceRow()][moveRequest.sourceColumn()];
 
-    public void destroyBlock(int row, int col)
-    {
-        board[row][col].setBlockType(Match3Block.BlockType.None);
-    }
+        board[moveRequest.sourceRow()][moveRequest.sourceColumn()] = board[moveRequest.targetRow()][moveRequest.targetColumn()];
+        board[moveRequest.targetRow()][moveRequest.targetColumn()] = temp;
 
-    public Match3Block[] findMatchedBlocks()
-    {
-        // TODO: Look into 2D Rabin-Karp
-        throw new UnsupportedOperationException("Not supported yet.");
+        return true;
     }
 
     public void destroyMatchedBlocks()
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<Match3Block> matches = findMatchedBlocks();
+
+        for (Match3Block match : matches)
+        {
+            destroyBlock(match);
+        }
     }
 
+    // TODO: Distinction between randomize and fill in (the latter should make blocks fall)
     public void fillInBlocks()
     {
         for (Match3Block[] row : board)
         {
             for (Match3Block block : row)
             {
-                if (block.getBlockType() == Match3Block.BlockType.None)
+                if (block.getBlockType() == Match3Block.BlockType.Empty)
                     block.setBlockType(randomBlockType());
             }
         }
     }
 
+    private List<Match3Block> findMatchedBlocks()
+    {
+        // TODO: Look into 2D Rabin-Karp because this is awful
+        List<Match3Block> matches = new ArrayList<>();
+
+        for (int i = 0; i < board.length; i++)
+        {
+            for (int j = 0; j < board[i].length; j++)
+            {
+                for (MatchableShape shape : matchableShapes)
+                {
+                    if (!blockMatchesShape(i, j, shape))
+                        continue;
+
+                    if (!matches.contains(board[i][j]))
+                        matches.add(board[i][j]);
+
+                    for (MatchableShape.RelativeCoordinates relativeCoordinates : shape.getRelativeCoordinates())
+                    {
+                        Match3Block block = board[i + relativeCoordinates.x()][j + relativeCoordinates.y()];
+
+                        if (matches.contains(block))
+                            continue;
+
+                        matches.add(block);
+                    }
+                }
+            }
+        }
+
+        return matches;
+    }
+
+    private void destroyBlock(Match3Block block)
+    {
+        block.setBlockType(Match3Block.BlockType.Empty);
+    }
+
     private Match3Block.BlockType randomBlockType()
     {
-        int randomBlockTypeValue = random.nextInt(1, Match3Block.BlockType.values().length); // Start from 1 so it ignores BlockType.None
+        int randomBlockTypeValue = random.nextInt(Match3Block.BlockType.Empty.ordinal() + 1, Match3Block.BlockType.values().length); // Start from 1 so it ignores BlockType.None
         return Match3Block.BlockType.values()[randomBlockTypeValue];
+    }
+
+    private boolean blockMatchesShape(int row, int column, MatchableShape shape)
+    {
+        if (!isInBounds(row, column))
+            return false;
+
+        Match3Block.BlockType thisBlockType = board[row][column].getBlockType();
+
+        if  (thisBlockType == Match3Block.BlockType.Disabled || thisBlockType == Match3Block.BlockType.Empty)
+            return false;
+
+        for (MatchableShape.RelativeCoordinates coordinates : shape.getRelativeCoordinates())
+        {
+            if (!isInBounds(row + coordinates.x(), column + coordinates.y()))
+                return false;
+
+            if (!(board[row +  coordinates.x()][column + coordinates.y()].getBlockType() == thisBlockType))
+                return false;
+        }
+
+        return true;
+    }
+
+    private boolean isInBounds(int row, int column) {
+        if (row < 0 || row >= board.length || column < 0 || column >= board[0].length)
+            return false;
+
+        return board[row][column].getBlockType() != Match3Block.BlockType.Disabled;
+    }
+
+    private boolean areBlocksSwappable(MoveRequest moveRequest)
+    {
+        int rowDistance = Math.abs(moveRequest.targetRow() - moveRequest.sourceRow());
+        int columnDistance = Math.abs(moveRequest.targetColumn() - moveRequest.sourceColumn());
+
+        return (rowDistance == 1 && columnDistance == 0) || (rowDistance == 0 && columnDistance == 1);
     }
 }
