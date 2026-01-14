b589f05f3912b3c31fab47b520a23e1c2661745d
Szymon Mucha <1197507@pw.edu.pl>
2026-01-14 17:48:32 +0100

    damage change

--- backend/src/main/java/pap/project/characters/impl/HoneyTrigger.java
+++ backend/src/main/java/pap/project/characters/impl/HoneyTrigger.java
@@ -1,19 +1,47 @@
 package pap.project.characters.impl;
 
+import org.springframework.lang.NonNull;
+import pap.project.characters.Character;
+import pap.project.characters.CharacterInGame;
 import pap.project.characters.model.CharacterType;
 import pap.project.game.match3.Match3Block;
+import pap.project.game_history.model.HistoryCharacterData;
 
-public class HoneyTrigger extends OneMainBlockCharacter {
-    //XXX
+import java.util.List;
+import java.util.OptionalInt;
+
+public class HoneyTrigger implements Character {
     private static final int BASE_HEALTH = 1000;
-    private static final int BASE_DAMAGE = 50;
+    private static final int BASE_DAMAGE = 40;
 
     private static final int HEALTH_PER_LEVEL_INCREASE = 100;
-    private static final int DAMAGE_PER_LEVEL_INCREASE = 10;
+    private static final int DAMAGE_PER_LEVEL_INCREASE = 5;
 
-    private static final Match3Block.BlockType MAIN_BLOCK = Match3Block.BlockType.CITRINE;
 
-    public HoneyTrigger() {
-        super(CharacterType.HONEY_TRIGGER, BASE_HEALTH, BASE_DAMAGE, HEALTH_PER_LEVEL_INCREASE, DAMAGE_PER_LEVEL_INCREASE, MAIN_BLOCK);
+    @Override
+    public final int getDamage(int level)
+    {
+        if (level < 1)
+            throw new IllegalArgumentException("Invalid level");
+        return BASE_DAMAGE + ((level - 1) * DAMAGE_PER_LEVEL_INCREASE);
+    }
+
+    @Override
+    public final int getHealth(int level)
+    {
+        if (level < 1)
+            throw new IllegalArgumentException("Invalid level");
+        return BASE_HEALTH + ((level - 1) * HEALTH_PER_LEVEL_INCREASE);
+    }
+
+    @Override
+    public final @NonNull OptionalInt getRequiredCopiesForNextLevel(int level)
+    {
+        return OptionalInt.of(level * 10);
+    }
+
+    @Override
+    public CharacterInGame createCharacterInGame(long characterId, int level) {
+        return new HoneyTriggerInGame(characterId, getDamage(level), getHealth(level), new HistoryCharacterData(CharacterType.HONEY_TRIGGER, level));
     }
 }
--- backend/src/main/java/pap/project/characters/impl/TrashMan.java
+++ backend/src/main/java/pap/project/characters/impl/TrashMan.java
@@ -6,10 +6,10 @@ import pap.project.game.match3.Match3Block;
 public class TrashMan extends OneMainBlockCharacter
 {
     private static final int BASE_HEALTH = 1000;
-    private static final int BASE_DAMAGE = 50;
+    private static final int BASE_DAMAGE = 150;
 
     private static final int HEALTH_PER_LEVEL_INCREASE = 100;
-    private static final int DAMAGE_PER_LEVEL_INCREASE = 10;
+    private static final int DAMAGE_PER_LEVEL_INCREASE = 30;
 
     private static final Match3Block.BlockType MAIN_BLOCK = Match3Block.BlockType.CITRINE;
 
